d7edaa98b500a931fcfe731179555da906e8c105
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-08-23 23:34:30 +0530

    changed

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 document.getElementById("demo").innerHTML = "your name is the khan ";
 document.write("my mane is the shivam ");
 alert("hellow owrld");
-console.log("hellow world");
+console.log("hellow world"); 
