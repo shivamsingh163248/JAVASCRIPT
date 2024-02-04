808afe3a0af458ab6447449dc09566c2451cd2b7
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-04 22:00:01 +0530

    update

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,2 +1,16 @@
 console.log("my name is shivam singh ");
 alert("what is you name ") ; 
+
+// now creating the variable in the java script 
+// three type of the variable found in the java script let const and var 
+let age = 20 ; 
+// now printing the variable in the console 
+console.log(age) ; 
+// if we are the creating the base pretices of the variable  
+let fistChileAge = 34 ; 
+let secondChildAge = 26 ; 
+
+// now printing the the console log 
+let sumOfChild = fistChileAge+secondChildAge ; 
+console.log(fistChileAge+secondChildAge) ; 
+console.log(sumOfChild) ;
