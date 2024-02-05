a8ae02a3c8a67cb1f11f3a3b30c7907844c99606
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-06 00:28:57 +0530

    update

---
+++ Javascript/typecorision.js
@@ -0,0 +1,29 @@
+// now check the type cohesion in the java script in the java script 
+
+let a = 6 + '6' ;
+console.log(a) ;  // this is the type of the string 
+// now checking the type of the a variable 
+console.log(typeof a ) ; // this is the showing the string 
+
+// ---------------- working ----------------------------
+
+// hear the concatenation  the value of the both in the form of the string 
+
+// given the example 
+let fristName = "shivam" ; 
+let secondName = "singh" ; 
+
+// now printing the both on the console 
+console.log(fristName+" "+secondName) ; 
+
+// now checking the example of the 
+
+let m = '5'*5 ; 
+console.log('5'*5) ; 
+// now checking the type of 
+console.log(typeof m  ) ; 
+
+// also checking the - 
+console.log('5' -5) ; 
+// answer will be the 0 ; 
+
