0fd2c5d472a4e4525b95c62d6cbd5a214298dfbe
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-05 22:38:44 +0530

    update

---
+++ Javascript/basicopreator.js
@@ -0,0 +1,36 @@
+/** Operators in JavaScript */
+/* 1. Arithemetic 
+      (+, -, *, /, %, **, ++, --) 
+*/
+let a = 10 ; 
+let b = 20 ; 
+let c = a+b ; 
+console.log(c) ; 
+console.log(10*20) ; 
+console.log(typeof c) ; 
+
+
+
+/* 2. Assignment 
+      (=, +=, -=, *=, /=, %=, **=) 
+*/
+
+
+
+
+/* 3. Comparison 
+      (<, > ,<=, >=, ==, !=, ===, !==)
+*/ 
+
+
+
+/* 4. Logical 
+      (&&, ||, !)
+*/
+
+
+
+
+/* 5. Bitwise 
+      (>>, <<, &, |, ^, ~)
+*/
