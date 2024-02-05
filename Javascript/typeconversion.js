e0ccb6804ad07014fd8a7f9d7b67aeb8c7958b34
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-06 04:26:55 +0530

    update

--- Javascript/typeconversion.js
+++ Javascript/typeconversion.js
@@ -26,4 +26,34 @@ console.log("number") ;
 // next method to convert in the number  using the number  
 // number()
 
+console.log(Number("290"));
+console.log(Number("290.09"));
+console.log(Number("shivam"));
+console.log(Number(true));
+console.log(Number(false));
+
+
+// now convening using the unary operator using + 
+let valu3 = '234' ; 
+let valu = '234shivam' ; 
+let valu2 = 'shivam343' ; 
+console.log(+valu) ; 
+console.log(+valu2) ;
+console.log(+valu3) ;
+
+
+// now converting with the phreaseflot and phraseint 
+
+console.log(parseInt("54.6f.54h")) ; 
+// now we can see not not directed 
+console.log(parseFloat("45.6fg.76")) ; 
+
+//all the string given the true without the empty 
+console.log(Boolean("")) ; 
+console.log(Boolean('')) ; 
+console.log(Boolean("shivam"))
+console.log(Boolean(1)) ; 
+console.log(Boolean(0)) ; 
+
+
 
