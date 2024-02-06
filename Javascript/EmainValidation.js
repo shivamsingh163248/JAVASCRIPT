b252ddc7682b2b77a9cb1a6d1c7a839ebe8b3332
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-07 02:27:18 +0530

    changed

--- Javascript/EmainValidation.js
+++ Javascript/EmainValidation.js
@@ -3,10 +3,24 @@
  // after the . only 2 and the 3 three Charter  are the allowed 
  // minium 3 chelator are allowed between . @
 
- const email  = prompt('enter the email') ; 
+  const email  = prompt('enter the email') ; 
 
- // fist checking the email length 
+  // fist checking the email length 
+// const email = "s@gil.com" ; 
+
   const emailLength = email.length ; 
  
   const emailDotIndex = email.lastIndexOf(".") ; 
-  // again find the 
+  // again find the the @  from the last index 
+  const emailsymboeleLength = email.lastIndexOf("@") ; 
+
+   console.log(emailLength) ; 
+   console.log(emailDotIndex) ; 
+   console.log(emailsymboeleLength) ; 
+
+
+   // now creating the condition if any condition is the true then the show the inhaled email 
+
+   if (emailLength < 11 ||  emailLength -  (emailDotIndex+1 )  < 2 ||  emailLength -  (emailDotIndex+1 )  > 3 ||emailDotIndex - emailsymboeleLength < 3) {
+     console.log("invaled email ")
+   }
