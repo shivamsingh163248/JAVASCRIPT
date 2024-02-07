c50c4ca735138c246bda9ffaeb45d5833345beb8
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-07 15:46:43 +0530

    learning the switch statmenet in js

---
+++ Javascript/switchStatement.js
@@ -0,0 +1,36 @@
+// now we are the learning the switch statement of the js 
+
+
+// switch statement expression is 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// } 
+
+// now taking the promet from the user using the browser 
+
+
+ const frute = "apples" ; 
+
+ // now creating the switch statement 
+
+ switch(frute) {
+case  "mango" :
+   console.log("this is the mango frute") ; 
+   break ; 
+case "apple" :
+    console.log("this is the apple frute") ; 
+    break ; 
+case "orange":
+    console.log("this is the orange") ; 
+    break ; 
+default :
+  console.log("this is the default line ") ; 
+  break ; 
+
+ }
