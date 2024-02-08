8c71a7abe15f79c515bc2a63c5ee3f3e2efc4a43
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-08 18:33:39 +0530

    number

---
+++ loopFunctionObject/loop.js
@@ -0,0 +1,21 @@
+
+// now crating the loop for the printing the number 
+let index = 0 ;
+for (; index < 10; index++) {
+  console.log(index) ; 
+    
+}
+
+// another the implement for loop 
+console.log("again checking the for loop ") ; 
+
+let indexs = 0 ;
+for (; indexs < 10; ) {
+  console.log(indexs) ; 
+  indexs++ ;
+    
+}
+
+
+
+//creating the do while loop 
---
+++ loopFunctionObject/whileloop.js
@@ -0,0 +1,13 @@
+
+// secreted the program using the while loop 
+
+let number = 54549958485 ; 
+
+// creating the do while loop 
+
+while (number != 0 ) {
+    
+    // find the remainder of the number 
+    console.log(number%10) ; 
+    number = parseInt(number/10) ; 
+}
