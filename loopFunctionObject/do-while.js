15b269026742ff15073c8e664b9c4440287dc8dc
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-08 18:51:42 +0530

    learning the do while loop in js

---
+++ loopFunctionObject/do-while.js
@@ -0,0 +1,10 @@
+
+
+// now creating the do -while loop 
+// fist run the do loop and then do while loop 
+
+let n = 1 ; 
+do {
+   console.log("print name is shivam singh "+`number is ${n}`) ; 
+ n++ ;  
+} while (n < 10);
