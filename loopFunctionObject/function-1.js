6b564037a876e59c3f290c409143e741a0e85a27
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-11 19:06:19 +0530

    update

--- loopFunctionObject/function-1.js
+++ loopFunctionObject/function-1.js
@@ -8,6 +8,15 @@ function isValid(str){
     }
 
     // now creating the loop 
+    for (let i = 0; i < str.length(); i++) {
+      
+        // check the all the place of  of string to validate the chreacture 
+        if (!((str[i] >= 'a' && str[i]<='z') || (str[i] >= 'A' && str[i] <= 'Z' ) ||(  str[i] === '_' ) ||( str[i] === '$') )) {
+            return false ; 
+        }
+        
+    }
+    return true ; 
 }
 
 
@@ -16,8 +25,17 @@ function isValid(str){
 function isValidIdentifier(identifire){
 
           
-   
+   // noe calling the above function for the checking the validation 
+   // we can also use the validation and the print the variable using the concatenation and the power symbol in the in java script 
  
+    // creating the the if else condition 
+    if (isValid(identifire) == true) {
+        
+        console.log(identifire+" is a valid identifier.");
+    }else{
+        console.log(identifire+" is not a valid identifier.");
+    }
+
 
 }
 isValidIdentifier("myVariable"); // Logs: myVariable is a valid identifier.
