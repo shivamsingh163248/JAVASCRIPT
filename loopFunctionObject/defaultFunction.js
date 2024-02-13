8e64cafb9548d39bfb8062bc210bbacc8777521e
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-13 15:43:50 +0530

    update

---
+++ loopFunctionObject/defaultFunction.js
@@ -0,0 +1,15 @@
+
+// creating the sum function with the default parameter 
+
+function sum(a , b){
+    return a+b ; 
+}
+
+// now calling the function and the test the function 
+
+let a  = 20 ; 
+let b = 30 ; 
+
+console.log(sum(a , b)) ; 
+// if i am calling with the single parameter 
+console.log(sum(a)) ; 
