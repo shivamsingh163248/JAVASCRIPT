cbb1634f95bc7d79edfa410a1303ecafa86520a0
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-14 07:00:02 +0530

    learning the charAt method

---
+++ string/chatAt.js
@@ -0,0 +1,9 @@
+
+// creating the method 
+
+let sentance = "my name is the shivam singh i am the b.tech student" ; 
+
+// now using the chatAt method 
+console.log(sentance.charAt(4)) ; 
+
+// chetAt the return the chat at the index numver
