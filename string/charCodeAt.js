b40d4ec0e522ebe9544f15bc09b1509f3ca92ed6
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-14 19:03:00 +0530

    update

---
+++ string/charCodeAt.js
@@ -0,0 +1,10 @@
+
+// creating the sentence 
+// and check the method of the charcode 
+
+let value = "my name is shviva singh " ; 
+// now testing the function of the  js 
+
+console.log(value.charCodeAt(0)) ; 
+
+// now we are the using charcode for the printing the value 
