a3c705c8a31c3e425afbd0abf42d8e07be68f663
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-14 06:35:21 +0530

    learing the return valu in js

---
+++ loopFunctionObject/returnStatement.js
@@ -0,0 +1,32 @@
+// now we are the learning the return method is js 
+// if we are not returning the any thing then if store value the value in any variable the result will be undefined 
+
+// creating the the function of the checking the odd and even 
+
+function checkNumer(number){
+    // creating the condition 
+    if (number%2 == 0) {
+        return 1 ; 
+    }else{
+        return 0 ; 
+    }
+}
+
+
+// now calling the function and the sotre the value in the another variable 
+
+const number = checkNumer(34) ; 
+console.log(number) ; 
+
+// if we are the creating the function not returning the any thing then the check the value 
+// result will be the undefine 
+
+function returnarry(){
+    
+    const value = ["shivam" , "itisha" , "apple" , true] ; 
+    console.log(typeof value) ; 
+    // now reversing the anything 
+}
+
+const returnvalues = returnarry() ; 
+console.log(returnvalues) ; 
