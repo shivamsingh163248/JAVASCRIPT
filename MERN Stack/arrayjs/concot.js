9800a5f78a9d7d9d8679b7a542d76d6940f9a13c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-16 04:46:55 +0530

    learning the concat method

---
+++ MERN Stack/arrayjs/concot.js
@@ -0,0 +1,9 @@
+
+// now we are the leaning the concat of the two array and the final print array 
+const arr1 = ["mango" , "orenge" , true] ; 
+const arr2 = new Array("satyendra kumar singh " , "sarita singh " , "lovely singh ") ;
+
+// now using the concatenating of the array 
+
+ const arr3 = arr1.concat(arr2) ; 
+ console.log(arr3) ; 
