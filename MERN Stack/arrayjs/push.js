c65e05fff4df35c545ca53b9256fb86b24882ee3
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-16 05:29:23 +0530

    learnign the pushing method in js

---
+++ MERN Stack/arrayjs/push.js
@@ -0,0 +1,16 @@
+
+
+// creating the an array and using the pushing method and the test  
+
+
+
+//--------------------------------push at last---------------------------
+
+const arr = new Array("mango" , "orange" , "apple" , 2 , true ,6) ; 
+
+// now we are the pushing the 
+arr.push("banana") ; 
+
+// now printing the array 
+console.log(arr) ; 
+
