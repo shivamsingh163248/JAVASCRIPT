0df488aed26025ef96fa625411e54c8db45113c9
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-16 05:20:57 +0530

    learning the pop method

---
+++ MERN Stack/arrayjs/pop.js
@@ -0,0 +1,7 @@
+
+// now creating the an array in the js 
+const arr = ["mango" , "pineapple" , " orange"] ; 
+console.log(arr) ; 
+const newArr = arr.pop() ; 
+console.log(newArr) ; 
+console.log(arr) ; 
