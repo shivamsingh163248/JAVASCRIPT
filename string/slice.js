28aaa3ac288f6e29071e79b4b8e5ae9005e2a1c4
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-15 15:41:19 +0530

    learning the slice method steing

---
+++ string/slice.js
@@ -0,0 +1,15 @@
+// slice(start, end)
+// substring(start, end)
+// substr(start, length)
+
+// slice have the two parameter 
+
+const sen = "my name is shivam singh and father name is satyendra kumar singh "
+//
+// using the splice 
+console.log(sen.slice(1,10)) ; // using for the between 
+console.log(sen.slice(5)) ;  // trim form the starting 
+console.log(sen.slice(-5)) ;  // showin form the end 
+console.log(sen.slice(-10,-1));  // it is form the between 
+
+
