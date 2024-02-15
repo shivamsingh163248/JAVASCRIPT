e86ef916ea8d469607ff2a4ca5b62322eef79ef9
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-16 05:24:35 +0530

    finding the length of the array

---
+++ MERN Stack/arrayjs/length.js
@@ -0,0 +1,9 @@
+// its check the the length of the array 
+const arr = [] ; 
+arr[0] = "shivam ";
+arr[1] = "mango"
+arr[3] = "orenge" ; 
+
+// now we are the finding the length using the array length method 
+console.log(arr.length) ;
+console.log(arr) ;  
