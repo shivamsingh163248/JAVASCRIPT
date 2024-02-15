58cc9b71aba771d8a5c90f66f56ab68ef882c98e
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-15 16:08:22 +0530

    leanring split

--- string/split.js
+++ string/split.js
@@ -1,2 +1,26 @@
 
 // split are the using for the converting the string into array 
+
+const sen = "my name is shivam singh "
+
+// creating the split of the sentence 
+console.log(sen.split("")) ; 
+
+// [
+//     'm', 'y', ' ', 'n', 'a',
+//     'm', 'e', ' ', 'i', 's',
+//     ' ', 's', 'h', 'i', 'v',
+//     'a', 'm', ' ', 's', 'i',
+//     'n', 'g', 'h', ' '
+//   ]
+
+console.log(sen.split(" ")) ; 
+
+//  'my', 'name', 'is', 'shivam', 'singh', '' ]
+
+//again test the split function 
+
+const test =" my,name,is,shivam" ; 
+console.log(test.split(","))
+
+
