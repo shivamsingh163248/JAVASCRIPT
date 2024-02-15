06faab5641c58615ec75c259d0b91c85f5f7bf5e
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-15 16:54:57 +0530

    learning the last index of the method

---
+++ MERN Stack/SearchStringMethod/indexOf.js
@@ -0,0 +1,15 @@
+
+// using for the indexof for the searching the string 
+
+const sen = "my name is 'shivam' singh  name " ; 
+
+// using the index of for the searching the string 
+// it is the return the position of the fist string 
+
+console.log(sen.indexOf("is"))
+console.log(sen.indexOf("shivani")) ; 
+
+// testing for the last index of the sting 
+
+console.log(sen.indexOf("name")) ; 
+console.log(sen.lastIndexOf("name")) ; 
