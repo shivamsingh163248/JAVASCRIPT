27e0415f309b69167fdbd81bc6647bff7b1cb00d
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-16 04:40:22 +0530

    leanrng the arrayafunction

---
+++ MERN Stack/arrayjs/arayInsilization.js
@@ -0,0 +1,33 @@
+
+// now we are the learning the array and initiation 
+if (1) {
+    console.log(false); 
+} else {
+    console.log(true); 
+}
+
+ const arr = ["mango" , "apple" ,"pineapple"  ] ; 
+ console.log(arr) ; 
+
+ // we are also see using the index number of the array 
+
+ console.log(arr[0]) ; 
+
+ // now we also initialization with the new keywoed of the array in js 
+
+ const newarr = new Array("mango" , "pine apple" , "orange" , "watermelon") ; 
+
+ // now check using the console method of the array 
+ console.log(newarr) ; 
+
+
+ // type 3 ----------------------------------------------------------------------------------
+
+ // fist initilization the array then set up the value 
+ const type3 = [] ; 
+type3[0] = "shivam" ; 
+type3[1] = "sarita singh " ; 
+type3[2] = "satyendra kumar singh " ; 
+
+// now checking on the console after the inisilazation of the array 
+console.log(type3) ; 
