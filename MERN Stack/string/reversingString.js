29aa1f8e3c5ad64e56b3bca41deb5574d18f060b
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-15 16:29:06 +0530

    learning js

---
+++ MERN Stack/string/reversingString.js
@@ -0,0 +1,14 @@
+
+const sen = "my name is shivam singh " ; 
+
+// now we are the want to remove the reversing all the string 
+
+// fist we are the split 
+const splitsent = sen.split(" ") ; // it convert to the array 
+
+// now we reversing the string using reversing method of the string 
+
+const reverisngString = splitsent.reverse() ; 
+
+// now agin join the spring each word are the reversing 
+console.log(reverisngString.join(" "));
---
+++ MERN Stack/string/trim.js
@@ -0,0 +1,15 @@
+
+// now we are the learning the trim 
+
+const sen = "   my    name is shivam singh " ; 
+
+// we want to trim the sentence of the sen and remove the white space 
+console.log(sen.length) ; 
+
+// trim return the new string after the trim 
+const afteratrim = sen.trim() ; 
+
+console.log(afteratrim.length) ; 
+
+// also we are the learning the start trim and end trim 
+
