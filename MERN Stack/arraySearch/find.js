0a17f90dadb8f44bef2d0edd4911c508646ebad6
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-16 06:36:58 +0530

    learning the last indexof

---
+++ MERN Stack/arraySearch/find.js
@@ -0,0 +1,6 @@
+
+
+// least index of the searching form the last of the element 
+const fruits = ["Apple", "Orange", "Apple", "Mango"];
+
+console.log(fruits.lastIndexOf("Apple")) // if not found then return -1 ;
