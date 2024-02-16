4cfa7a72fca5f014b72a44f4cc01e578b84b0eee
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-16 06:01:19 +0530

    learning slice method

---
+++ MERN Stack/arrayjs/deleted.js
@@ -0,0 +1,7 @@
+
+
+// deleted not a shifting it given the blank 
+// 
+const arr = ["my" , "name" , "is", "shivam" ,"singh"] ; 
+delete arr[0] ; 
+console.log(arr) ; 
---
+++ MERN Stack/arrayjs/slice.js
@@ -0,0 +1,6 @@
+
+
+// now we are the learning the splice method for the 
+// it using the remove from the font print the after the index 
+const arr = ["my" , "name" , "is", "shivam" ,"singh"] ; 
+console.log(arr.slice(3));
