864b2db5e2048c8b61ebe69a4a47e7a19899d0da
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-16 05:55:12 +0530

    join method using to convert arr to string

---
+++ MERN Stack/arrayjs/join.js
@@ -0,0 +1,9 @@
+
+
+// join method using for to change to string
+const arr = ["my" , "name" , "is", "shivam" ,"singh"] ; 
+
+// it changes to string 
+console.log(arr.join()) ; 
+console.log(arr.join(""))
+console.log(arr.join(" "))
