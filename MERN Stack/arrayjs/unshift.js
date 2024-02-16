26212123a07feb46915299d26255315529e2bdef
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-16 05:49:18 +0530

    learning the unshift function

---
+++ MERN Stack/arrayjs/unshift.js
@@ -0,0 +1,12 @@
+
+// using the unshift function that are the using for the insert from the start 
+
+// now creating the array 
+const arr = new Array("shivam" , "mango" , 3 , 5, true , false) ; 
+
+// creating the unshif function add at front 
+
+const arr1 = arr.unshift("itisha") ; 
+
+//checking at console 
+console.log(arr) ; 
