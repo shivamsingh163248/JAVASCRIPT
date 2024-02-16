aee9cb05f419d3553d4ddb7fa12029ede184095c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-16 06:34:53 +0530

    learnign the indexof methiod

---
+++ MERN Stack/arraySearch/indexof.js
@@ -0,0 +1,8 @@
+
+
+//find the index of the array 
+const fruits = ["Apple", "Orange", "Apple", "Mango"];
+// now checking the index 
+console.log(fruits.indexOf("Apple")) ; 
+// IT CHECK THE stickely 
+console.log(fruits.indexOf("apple")) ; 
