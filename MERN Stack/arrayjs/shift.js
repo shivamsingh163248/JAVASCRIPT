649b29816699cffb4cfba81d1b6f07111628b8aa
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-16 05:44:15 +0530

    learning the shift remove

---
+++ MERN Stack/arrayjs/shift.js
@@ -0,0 +1,9 @@
+
+
+// now learning the shift method    
+
+//-------------------------------removing form the start -------------------------------
+
+const arr = ["shivam" , true , "mango" , 4] ; 
+ const arr1 = arr.shift() ; 
+ console.log(arr) ; 
