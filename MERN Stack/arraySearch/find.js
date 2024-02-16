e22794a037d452673bbbb467b929cfcd412a1db0
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-16 06:49:23 +0530

    learing the array search method incude and indexof and lastindexof

--- MERN Stack/arraySearch/find.js
+++ MERN Stack/arraySearch/find.js
@@ -4,3 +4,13 @@
 const fruits = ["Apple", "Orange", "Apple", "Mango"];
 
 console.log(fruits.lastIndexOf("Apple")) // if not found then return -1 ;
+
+// again check the find method 
+const numbers = [4, 9, 16, 25, 29];
+let first = numbers.find(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 18;
+}
+
+console.log(first)
---
+++ MERN Stack/arraySearch/include.js
@@ -0,0 +1,8 @@
+
+// including providing the boolean value 
+// now creating the array using the new keyword in js 
+
+const arr = new Array("mango" , "banana" , "apple" , "papaya") ; 
+// now we are the searching using the include method in js 
+console.log(arr.includes("banana")) ; 
+console.log(arr.includes("Apple")) ; 
