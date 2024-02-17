153e9ef0bbd9c9da9e43db465f4079e66843213a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-17 14:45:12 +0530

    learnign the forofloop

---
+++ MERN Stack/loops/forOfLoop.js
@@ -0,0 +1,10 @@
+
+
+const fruits  = new Array("banana" , "apple" , "pine apple" , "lemone" , "gavava") ; 
+
+// leaning the forefoot 
+
+for (const key of fruits) {
+    
+    console.log("fruts name is : "+key) ; 
+}
