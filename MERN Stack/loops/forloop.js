ece622de3586cab3e59a3d3bd54048af3a77c2cd
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-17 14:21:48 +0530

    learing the for loop

---
+++ MERN Stack/loops/forloop.js
@@ -0,0 +1,10 @@
+
+// creating the an array 
+const fruits = ["apple" , "banana" , "watermelon" , "lemon" , "guava"] ; 
+
+// now using the for loop for the access element 
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log("fruits : "+fruits[i]) ; 
+    
+}
