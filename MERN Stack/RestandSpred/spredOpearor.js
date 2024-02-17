8c3d206a9d89c328546e6a63e164ee0cb4fb5bdc
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-17 18:14:21 +0530

    learning spred operator

---
+++ MERN Stack/RestandSpred/spredOpearor.js
@@ -0,0 +1,3 @@
+
+// this is the array 
+const fruits  = new Array("banana" , "apple" , "pine apple" , "lemone" , "gavava") ; 
---
+++ MERN Stack/loopFunctionObject/functionReturnjs.js
@@ -0,0 +1,24 @@
+
+function sun(){
+    let a = 20 ; 
+    let b = 30 ; 
+
+    return a,b ; 
+}
+
+var a,b = sun() ; 
+console.log(a,b) ; 
+console.log(sun()) ;// always the last value return in js that are the speared by the , 
+
+// function sum(){
+//     let a = 20 ; 
+//     let b = 30 ; 
+
+//     return ab ; 
+// }
+
+// var a,b = sum() ; 
+// console.log(a,b) ; 
+// console.log(sum()) ;
+
+
