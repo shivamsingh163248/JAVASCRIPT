8b7aafef989b11a05c9bfa8161b52ae77baf6b63
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-17 14:40:08 +0530

    learning the for in loop

---
+++ MERN Stack/loops/forInLoop.js
@@ -0,0 +1,11 @@
+
+ 
+ // creating the array 
+
+ const fruits  = new Array("banana" , "apple" , "pine apple" , "lemone" , "gavava") ; 
+
+ // creating the loop of the for in loop that given the index number like the for each loop 
+
+ for (const key in fruits) {
+   console.log(`fruts numbers is ${key}   ` + fruits[key]) ; 
+ }
