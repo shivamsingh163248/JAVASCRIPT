2edf73ec33e0e1bad9720a2348fc33c6f0674339
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-17 18:35:51 +0530

    learnign the spred object

--- MERN Stack/RestandSpred/spredOpearor.js
+++ MERN Stack/RestandSpred/spredOpearor.js
@@ -1,3 +1,23 @@
 
 // this is the array 
 const fruits  = new Array("banana" , "apple" , "pine apple" , "lemone" , "gavava") ; 
+const vaeg = ["branjil" , "cally flower" , "redis" , "carrot" ] ; 
+const anuimal = ["loin" , "elephent" , "rabbit"]  ; 
+
+// now we are the pushing element in fruts ; 
+
+
+
+// now printing on the console 
+console.log(fruits) ; 
+
+const newarr = [...fruits] ;  // hear is not copy address hear the generated the new address 
+fruits.push("new element") ; 
+console.log(fruits) ; 
+console.log(newarr) ; 
+
+ // if want to contat 
+ const mergeCreature = [...anuimal , ...fruits] ; 
+ console.log(mergeCreature) ; 
+
+
