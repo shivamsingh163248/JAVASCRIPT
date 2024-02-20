7d4a0e58e3df0280a415b2fc214378686543bfd9
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-20 09:37:38 +0530

    update

--- MERN Stack/ExcuationContextDevTool/jsworktest.js
+++ MERN Stack/ExcuationContextDevTool/jsworktest.js
@@ -1,1 +1,20 @@
 // now writing the program 
+var names = "shivam" ;
+console.log(`nam2 :  ${names}`) ;
+let age = 21 ;
+console.log(`age :  ${age}`) ;
+let address = "varanshi" ;
+
+// creating the function for the wishing the birthday
+function wishBirthday( address){
+    let name = "shivam" ;
+    let age = 21 ;
+    console.log(`Happy Birthday ${name} and your age is ${age} and your address is ${address} `) ;
+
+    return 2010 ; 
+
+}
+
+// declared the another variable 
+let result = wishBirthday(address) ;
+console.log(`result ${result}`) ; 
