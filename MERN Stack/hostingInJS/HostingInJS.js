27196bddea103d2545d650930b4681f2896096ad
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-22 00:30:22 +0530

    update

--- MERN Stack/hostingInJS/HostingInJS.js
+++ MERN Stack/hostingInJS/HostingInJS.js
@@ -7,21 +7,29 @@ console.log(value1) ;
 var value2 = ` my father name is ${fatherName} and he is a good person` ;
 console.log(value2) ;
 
- var myname = "shivam singh" ;
-    var fatherName = "Satyendra kumar singh " ;
+ var Myname = "shivam singh" ;
+ var fatherName = "Satyendra kumar singh " ;
 
-    var value1 = `my name is ${Myname} and i am learning the hosting in js` ; 
-console.log(value1) ;
-var value2 = ` my father name is ${fatherName} and he is a good person` ;
-console.log(value2) ;
+    var value3 = `my name is ${Myname} and i am learning the hosting in js` ; 
+console.log(value3) ;
+var value4 = ` my father name is ${fatherName} and he is a good person` ;
+console.log(value4) ;
 
 // now calling the some function for the learning the hosting in js 
  address() ; 
 
  // now creaitng the function for the learning the hosting in js
     function address() {
-        var Myname = "Rahul" ; 
-        var fatherName = "Rajesh" ; 
-        console.log(`my name is ${Myname} and i am learning the hosting in js`) ;
-        console.log(` my father name is ${fatherName} and he is a good person`) ;
+        var Mynames = "Rahul" ; 
+        var fatherNames = "Rajesh" ; 
+        console.log(`my name is ${Mynames} and i am learning the hosting in js`) ;
+        console.log(` my father name is ${fatherNames} and he is a good person`) ;
+        var year = 2021 ; 
+        console.log(`the year is ${year}`) ;
+        return year ;
     }
+
+
+    // here we are calling the function before the function is created
+    var yearvalue = address() ; 
+    console.log(`the year is ${yearvalue}`) ;
