08428dae795ab68c453daf1ae116a3a3629cf37a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-22 00:08:10 +0530

    update

---
+++ MERN Stack/hostingInJS/HostingInJS.js
@@ -0,0 +1,27 @@
+// creating the function of the learning the hodting in js 
+
+// creating the var this is the global var this is not a blok scope var 
+
+var value1 = `my name is ${Myname} and i am learning the hosting in js` ; 
+console.log(value1) ;
+var value2 = ` my father name is ${fatherName} and he is a good person` ;
+console.log(value2) ;
+
+ var myname = "shivam singh" ;
+    var fatherName = "Satyendra kumar singh " ;
+
+    var value1 = `my name is ${Myname} and i am learning the hosting in js` ; 
+console.log(value1) ;
+var value2 = ` my father name is ${fatherName} and he is a good person` ;
+console.log(value2) ;
+
+// now calling the some function for the learning the hosting in js 
+ address() ; 
+
+ // now creaitng the function for the learning the hosting in js
+    function address() {
+        var Myname = "Rahul" ; 
+        var fatherName = "Rajesh" ; 
+        console.log(`my name is ${Myname} and i am learning the hosting in js`) ;
+        console.log(` my father name is ${fatherName} and he is a good person`) ;
+    }
---
+++ MERN Stack/hostingInJS/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+var value1 = `my name is ${Myname} and i am learning the hosting in js` ; 
+// console.log(value1) ;
