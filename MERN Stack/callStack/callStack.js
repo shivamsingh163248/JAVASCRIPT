959202fef913793509f5eac925dde43e6044285b
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-23 21:13:05 +0530

    update

--- MERN Stack/callStack/callStack.js
+++ MERN Stack/callStack/callStack.js
@@ -6,17 +6,32 @@ var address = "nadhihar rajgarh mirzapur" ;
 
 // now we are the both printing on the console for the checking 
 
-let age = 20 ;
 
+let age = 20 ;
+    
  console.log(`name is ${username}`);
     console.log(`address is ${address}`); 
 
 // creating the function wising happy birthyday 
  function whishingHappyBirthday(age){
      console.log(`happy birthday ${username}`);
-
+     let values = 10;
+     console.log(values+20) ; 
     
      if (age > 18){
      happybirthday();
      }
+ }
+
+
+ // creating the happy birthday function 
+
+ function happybirthday(){
+
+
+    let number = 10;
+        console.log(`happy birthday ${username}`);
+        console.log(`your age is ${age}`);
+        console.log(`your number is ${number}`) ; 
+
  }
