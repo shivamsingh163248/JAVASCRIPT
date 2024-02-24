909718c4886b179e27520642d19ffd1a7e567bb4
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-24 06:51:01 +0530

    update

---
+++ MERN Stack/Scope/undifinedNotDefined.js
@@ -0,0 +1,42 @@
+// creating the 
+let d  ; 
+console.log(d) ;
+
+// NOW TESTING UNDEFINED VARIABLE 
+
+console.log(a) ;
+var a = 10 ;
+
+//--------------- showing the error -----------------//
+// console.log(a) ;
+//  a = 10 ;
+
+c = 30 ; 
+console.log(c) ;
+
+//--------------- showing the error -----------------//
+// console.log(g) ;
+// g = 30 ; 
+
+
+
+
+
+// checking the temporal dead variable 
+//--------------- showing the error -----------------//
+// let check  =30 ; 
+// // creating the scope 
+// {
+//     console.log(check) ; 
+//     let check = 20 ; 
+     
+// }
+
+
+let check = 30 ; 
+{
+    console.log(check) ; 
+    
+}
+
+
