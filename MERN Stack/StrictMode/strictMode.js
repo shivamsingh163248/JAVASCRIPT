acdb5652e62bd4d6ee20be5fd46b83a806badd6c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-24 20:34:50 +0530

    update

---
+++ MERN Stack/StrictMode/strictMode.js
@@ -0,0 +1,22 @@
+// learning the string mode how thing are happening in the strict mode 
+// declearing the variable 
+
+'use strict' ;
+let value = 10 ; 
+// creating the condition
+if(true){
+    valeu = 20 ; 
+    console.log(value) ;
+}
+
+
+ // -------- second type --------------
+
+// creating the condition
+if(true){
+    'use strict' ;
+    valeu = 20 ; 
+    console.log(value) ;
+}
+
+
---
+++ MERN Stack/StrictMode/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+'strict mode'
+let value = 10 ; 
+// creating the condition
+if(true){
+    valeu = 20 ; 
+    console.log(value) ;
+}
