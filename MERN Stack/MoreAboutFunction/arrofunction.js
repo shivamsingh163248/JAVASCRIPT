8155889920d29d336419b42ba11909865953d21c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-26 07:40:02 +0530

    update

--- MERN Stack/MoreAboutFunction/arrofunction.js
+++ MERN Stack/MoreAboutFunction/arrofunction.js
@@ -6,6 +6,7 @@ const sum = (a,b) => {
 
 
 // arrow function replace function keword 
+// that is the implicitly function 
 
 const multiply = (a ,b) => a*b ; 
 
@@ -18,3 +19,38 @@ console.log(sumfunction) ;
 
 let multiplyfunction = multiply(3,5) ; 
 console.log(multiplyfunction) ; 
+
+// all the function work properly 
+
+// check function all balank parameter 
+const checkFun = (() =>{
+    // crating the another function 
+    let x = 5 ; 
+    return () =>{
+        x += 1 ; 
+        return x ; 
+    };
+}
+) () ; 
+console.log(checkFun) ;
+
+
+const number = check =>{
+    return "shivam"+check ; 
+}
+
+// now calling the function and printing on the console 
+console.log(number("singh")) ;
+
+// check the can possible two or the more returning 
+
+
+function morereturnfunction(){
+   let  num = 20 ; 
+    let check = 50 ; 
+    let sums = 10 ; 
+    return  {num , check , sums} ; 
+}
+
+// now invoking the function 
+console.log(morereturnfunction()) ; 
