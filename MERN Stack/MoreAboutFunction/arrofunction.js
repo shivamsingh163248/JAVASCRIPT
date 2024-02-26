8a72df965251d371dc428136650e0e41a9cb3192
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-26 06:20:35 +0530

    learning the arrow function

---
+++ MERN Stack/MoreAboutFunction/arrofunction.js
@@ -0,0 +1,20 @@
+// now learning the arrow function in the js 
+
+const sum = (a,b) => {
+    return a+b ; 
+}
+
+
+// arrow function replace function keword 
+
+const multiply = (a ,b) => a*b ; 
+
+
+// now calling the function both function and test  the arrow function 
+
+let sumfunction = sum(3,5) ; 
+// now consoling the output the value 
+console.log(sumfunction) ; 
+
+let multiplyfunction = multiply(3,5) ; 
+console.log(multiplyfunction) ; 
