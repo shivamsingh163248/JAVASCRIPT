81e04b54071cfaa257fe2a62fcb10e44c6aa42db
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-26 08:21:20 +0530

    learning more about the scope

---
+++ MERN Stack/Scope/globle1.js
@@ -0,0 +1,12 @@
+// creating the var variable 
+
+var a = 40  ; 
+
+// creating the  function to update the variable 
+function update(){
+    a += 1 ;
+     return a  ;  
+}
+
+// now again calling the function 
+
---
+++ MERN Stack/Scope/globle2.js
@@ -0,0 +1,13 @@
+// ccalling the function 
+
+console.log(update()) 
+
+// now update the value 
+// creating gain update the function 
+console.log(sumupdate()) ; 
+
+function sumupdate(){
+    a += 100 ; 
+    return a ; 
+}
+
