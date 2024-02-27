9e5d091ffac57d63678803fe54dc0098e0d30564
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-27 22:34:32 +0530

    pdate

---
+++ MERN Stack/IIFE/TestElif1.js
@@ -0,0 +1,12 @@
+// now learning the function of the iilf and creating  the function and test how actually are the working of the iifi
+
+// creating the function and function writing the iifi code 
+function main(){
+
+    return (function(){
+        console.log("hello world") ; 
+    })() ; 
+}
+
+//now calling the main function 
+main() ; 
