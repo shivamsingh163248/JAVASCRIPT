e836797a2fcc7a53b4ff99743cdc648015b2ecb0
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-02 06:33:54 +0530

    update

---
+++ MERN Stack/MoreAboutFunction/mapFunction.js
@@ -0,0 +1,29 @@
+// now learning the map function 
+// map() creates a new array from calling a function for every array element.
+
+// map() does not execute the function for empty elements.
+
+// map() does not change the original array. 
+const frusts = ["orange" , "graps" , "watermelon" , "apple" , "patato"] ; 
+ const num = [2,4,5,6,7,6,5,4,2,5,7,34,6,46,324,46,53,6,3,6] ; 
+
+ // map function are the using the modification into the array that are the map function 
+
+
+ const cheange = frusts.map(function(currentarr){
+    return currentarr+"ing" ; 
+ })
+ 
+ console.log(cheang) ; 
+
+ // if an calling the map function and not returning any thing in the function 
+ // then check the what will the return the function 
+ const cheang = frusts.map(function(currentarr){
+    
+ })
+ console.log(cheang) ; 
+
+
+
+ //*************************************************************************************** */
+ 
