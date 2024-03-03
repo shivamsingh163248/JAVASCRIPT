3ee447f2465585a99ce4adb9ec741beebcc9120f
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-03 21:13:16 +0530

    learning the reducing function

--- MERN Stack/MoreAboutFunction/reduceFunction.js
+++ MERN Stack/MoreAboutFunction/reduceFunction.js
@@ -12,8 +12,19 @@
  const arr = [3,4,5,6,78,5,3,24,2] ; 
 
  function adding(accumulator , currentVlue , currentIndex ){
-    return `${accumulator+currentVlue} index : ${currentIndex} => ` ; 
+     return `${accumulator+currentVlue} index : ${currentIndex} => ` ; 
  }
 
  const result = arr.reduce(adding) ; 
  console.log(result) ; 
+
+
+
+ // again creating the another function and the check the reducing the function 
+ const test = arr.reduce((accumulatorstoring , currentvalue  , currentIndex) => {
+     console.log( `accumlator ${accumulatorstoring} `+currentIndex) ;
+     return accumulatorstoring+currentvalue ; 
+
+ });
+
+ console.log(test) ; 
