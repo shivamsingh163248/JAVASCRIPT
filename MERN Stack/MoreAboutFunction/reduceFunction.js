7ad153782b1a77d892dc90be2466ff56439b7678
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-03 21:00:52 +0530

    update

--- MERN Stack/MoreAboutFunction/reduceFunction.js
+++ MERN Stack/MoreAboutFunction/reduceFunction.js
@@ -1,1 +1,19 @@
-// now learning the reduce fucntion 
+// now learning the reduce fucnti
+// reduce function have the three parameter 
+
+// reduce function hava the two value 
+
+//  reduce(reduceFun , insialvalue)
+
+// reducation function have the four  parameter 
+ // reudcefunction(accumulator  , currentVlue , currentIndex , arr) 
+
+
+ const arr = [3,4,5,6,78,5,3,24,2] ; 
+
+ function adding(accumulator , currentVlue , currentIndex ){
+    return `${accumulator+currentVlue} index : ${currentIndex} => ` ; 
+ }
+
+ const result = arr.reduce(adding) ; 
+ console.log(result) ; 
