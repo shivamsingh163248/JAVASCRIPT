25cad1e33c06d4578ecadb4ae0e26516df4b9172
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-04 14:48:33 +0530

    update

--- MERN Stack/JavascriptFundamental/FilterFunction.js
+++ MERN Stack/JavascriptFundamental/FilterFunction.js
@@ -1,1 +1,6 @@
 // now learning the filter function 
+const arr = [4,6,2,-5,6,4,2,5,3,77,3] ; 
+
+// now simple apply the filter function 
+const result = arr.filter((arrs)=> {return arrs > 5 }) ; 
+console.log(result) ; 
