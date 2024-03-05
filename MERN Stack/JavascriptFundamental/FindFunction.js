88bc2618ac805230a6b4aaf0b9878edba942350a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-05 11:43:55 +0530

    update

---
+++ MERN Stack/JavascriptFundamental/FindFunction.js
@@ -0,0 +1,8 @@
+const arr = [2,4,52,56,2,64,56,2,6,2,6,2,6,2,65,2,45,24,6,3,5] ; 
+
+// now learning the find function 
+const result = arr.find((currentvalue) => currentvalue > 7) ; 
+console.log(result) ;
+const result1 = arr.find((currentvalue) => currentvalue == 7) ; 
+console.log(result1) ;  
+
