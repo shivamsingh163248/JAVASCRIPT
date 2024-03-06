48e374364290e3eff5a1e22a72a7e724990e4db8
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-07 04:05:10 +0530

    we are the also writing the second type of way of the writing of the function

--- MERN Stack/OOP/CreatingInvokeFunction.js
+++ MERN Stack/OOP/CreatingInvokeFunction.js
@@ -14,7 +14,7 @@ const details = {
         const post = "rajgarh" ; 
         const dist = "Miz" ;
 
-        console.log(`${details.username} is the address is ${vill} , ${post} , ${dist}`) ; 
+        console.log(`${this.username} is the address is ${vill} , ${post} , ${dist}`) ; 
 
     },
 
@@ -40,3 +40,32 @@ console.log(details.result.fisryear) ;
 const yearname = "fourtyear" ; 
 
 console.log(details.result[yearname]) ; 
+
+
+
+
+// thw new way of the writing of using this keyword and and using the direct writing the function not required the function name 
+const detail = {
+    username : "shivam singh" , 
+    class : "b.tech"  , 
+    branch : "CSE",
+    session : "2020-24" , 
+
+    address(){
+        const vill = " nadiahr" ;
+        const post = "rajgarh" ; 
+        const dist = "Miz" ;
+
+        console.log(`${details.username} is the address is ${vill} , ${post} , ${dist}`) ; 
+
+    },
+
+    // again crating the key and contained the 
+    result : {
+        fisryear : 65 , 
+        secondyear : 70 , 
+        thridyear : 75 , 
+        fourtyear : 80 
+
+    }
+}
