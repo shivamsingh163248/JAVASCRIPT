cbd89e4654e2120f806746a3855daa138ac9faff
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-07 05:11:23 +0530

    counstorctur based learning creating object

---
+++ MERN Stack/OOP/CreatingObjectUsingCounstoctur.js
@@ -0,0 +1,34 @@
+// hear we are the learning the method of the coustoctur based oop 
+
+// pont 1 ; function name fist latter should be capital latter 
+// pont 2 : if we are the creating the function the using the name this keyword of the function 
+// creating the time of generation of the function 
+
+
+
+// now lets learn how actually created the coustoctur in the java scripts 
+
+function Movie(moviesName , MoviesTitle){
+
+  this.moviesName = moviesName ; // this should be use the semicolon at that time of the counstoctur inisalization 
+  this.MoviesTitle = MoviesTitle ; 
+
+  this.getDetails = function() {  // this is the not have the name 
+        
+    console.log(` moviesName ${this.moviesName} movies title : ${this.MoviesTitle}`) ; 
+
+  }
+
+}
+
+
+// now creating the object using the 
+
+const movies1  = new Movie("kgf" , "south") ; 
+const movies2 = new Movie("SLR" , "SOUTH") ; 
+
+
+movies1.getDetails() ; 
+movies2.getDetails() ; 
+
+// this is the working the perfectly of the counstroctur based creating object 
