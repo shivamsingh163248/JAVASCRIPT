53b1efd15d5c2453ebb223abf3ecc6875a55f281
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-07 14:50:06 +0530

    sayentx of writing of prototype

---
+++ MERN Stack/OOP/PrototypeWriting.js
@@ -0,0 +1,8 @@
+function Person(name) {
+    this.name = name;
+    }
+    Person.prototype.age = 30;
+    const john = new Person("John");
+    john.__proto__.age = 40;
+    console. log(john.age);
+    console. log(Person. prototype. age);
