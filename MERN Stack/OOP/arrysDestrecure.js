c5b9ca4dd9459626fbc3e35f15f364526137a86a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-09 16:49:22 +0530

    learning the destrectruing

---
+++ MERN Stack/OOP/arrysDestrecure.js
@@ -0,0 +1,9 @@
+// now learning the arrays Destructuring
+
+const fruits = ["apple" , "orange" , "pathos" , "guava" , "banana" , "watermelon" , "lemon" , "lichi"] ; 
+
+// now learning the arras the destructuring 
+
+const[a,,b] = fruits ; 
+console.log(a) ; 
+console.log(b) ; 
