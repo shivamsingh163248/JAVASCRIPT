5105b9e339cfe6ebe25cd30ee83fe1a7dee3f0e8
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-09 16:10:35 +0530

    learning the destructuring and renaming

--- MERN Stack/OOP/DestrecturingRenaming.js
+++ MERN Stack/OOP/DestrecturingRenaming.js
@@ -1,1 +1,27 @@
-// now again 
+// now again learning the renaming Destructuring  
+  
+const obj = {
+    a : 4 , 
+    b : [4,65,4,4,2,4,5] , 
+    value :{ name:"shivam singh" , address : "nadiahr rajgarh mirzapur"} , 
+    result : function(){console.log("learning the Destrecturing ")} , 
+}
+
+
+// now we are the de structuring of the object and remaining 
+
+const{a:four} = obj ; 
+console.log(four) ;
+
+const {value:Details} = obj ; 
+
+// now again check the 
+console.log(Details) ; 
+
+
+// assign pattern 
+// fist we are the assign value then implement 
+
+let b1 ; 
+({b:b1} = obj );  // this is the way of the writing to code 
+console.log(b1) ; 
