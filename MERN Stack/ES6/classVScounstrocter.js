d8b2f6e8dd60af7f2d2bdfe53a740794d4aceb8c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-11 23:14:25 +0530

    learning the class in the js

---
+++ MERN Stack/ES6/classVScounstrocter.js
@@ -0,0 +1,14 @@
+// now learning the class and difference between them 
+// creating the class name is the class but in creating the normal coustoctur   
+
+// now creating the constructor 
+function Student(username , userDetals ){
+    this.username = username ; 
+    this.userDetals = userDetals ; 
+    this.getDetails = function(){
+        console.log(`this is username : ${this.username} and user Details : ${this.userDetals}`) ; 
+    }
+}
+
+
+// now learning the class how to actually implement class 
