3124245192923a4c6606d809b53db194d86a84ed
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-18 20:27:34 +0530

    update

--- MERN Stack/ES6/CounstorcterFunctionInheritance.js
+++ MERN Stack/ES6/CounstorcterFunctionInheritance.js
@@ -6,17 +6,24 @@ function Vehikals(VehikalsNmae , vahikalsType , vehikalNumber){
 
 
    // creating the function of the getDeials 
-   this.getdetails = function(){
+   this.getdetails = function(name){
 
     // all the details of the vehikals are printid on the console
     console.log(`vehikals name is ${this.VehikalsNmae} and the type is ${this.vahikalsType} and the number is ${this.vehikalNumber}`) ;
-    
+    console.log(name) ; 
  
    };
 
  
 
 }
+
+Vehikals.prototype.getAllDetails = function(){
+    console.log("This is the  prototype") ; 
+}
+
+ 
+
 
 // creating another counstoctor another function 
 // and using vehikal in the class 
@@ -26,13 +33,25 @@ function Car(CarName , CarHub){
     this.CarName = CarName ; 
     this.CarHub = CarHub  ; 
 
+    // now using the calling the function 
+    Vehikals.call(this , "4 viller" , "disul" , "4545") ; 
+
     // if want to user 
     // using the call function using the this property to user like inheritance 
-    console.log(`vehikals name is ${this.VehikalsNmae} and the type is ${this.vahikalsType} and the number is ${this.vehikalNumber}`) ;
-    console.log(`CAR NAME ${this.CarName} carhub ${this.CarHub}`) ; 
+    this.getdetails = function(name){
 
+        // all the details of the vehikals are printid on the console
+        console.log(`vehikals name is ${this.VehikalsNmae} and the type is ${this.vahikalsType} and the number is ${this.vehikalNumber}`) ;
+        console.log(name) ; 
+     
+       };
 
 }
 
 // creating the object of the car 
-const obj = new Car("Z-20" , "toytaShow room") ; 
+const obj = new Car("Z-20" , "toytaShow room") ;
+
+
+console.log(obj) ;
+obj.getdetails("shivam ") ; 
+
