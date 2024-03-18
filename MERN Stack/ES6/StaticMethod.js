52b15dc713a4d14280e78f78bfc618ab77a26279
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-18 23:20:11 +0530

    learning the static method

--- MERN Stack/ES6/StaticMethod.js
+++ MERN Stack/ES6/StaticMethod.js
@@ -8,7 +8,20 @@ constructor(username){
 
 // creating the static method 
 static printDetails(){ // you car write  the method like the 
+   console.log("this is the static method ") ; 
+   console.log("you can check the static method " ) ; 
+}
+
+Details(){
+    console.log("this is the normal method ") ; 
 
 }
 
+typeDetailsMehtod = function(){
+    console.log("this is the the changed the type method ") ; 
 }
+
+
+}
+
+// now creating the 
