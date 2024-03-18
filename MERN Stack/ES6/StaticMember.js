5853196229a05b61fcd29577a6b40dbc2fccc025
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-18 23:40:30 +0530

    learning the static memeber

--- MERN Stack/ES6/StaticMember.js
+++ MERN Stack/ES6/StaticMember.js
@@ -4,10 +4,22 @@ class Details{
     // creating the coustoctur 
     username ; 
     userclass ; 
-    static value ; 
+    static value = "static name " ; 
     constructor(username , userclass , value){
         this.username = username ; 
         this.userclass = userclass ; 
-        this.value = value ; 
+       
     }
+
+
 }
+
+// creating the object of the these class 
+const checkObj = new Details("shivam" , "b.tech" , "343") ; 
+console.log(checkObj) ; 
+console.log(checkObj.userclass) ; 
+console.log(checkObj.value) ; 
+
+// check the value using invoking with the class 
+console.log(Details.value) ; 
+
