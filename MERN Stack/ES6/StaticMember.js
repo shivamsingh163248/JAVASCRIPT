59daa0d54fc25294b2b7d42041d39df9b0fcc3db
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-18 23:32:53 +0530

    learning the static memeber

---
+++ MERN Stack/ES6/StaticMember.js
@@ -0,0 +1,13 @@
+// now learning the private member 
+class Details{
+
+    // creating the coustoctur 
+    username ; 
+    userclass ; 
+    static value ; 
+    constructor(username , userclass , value){
+        this.username = username ; 
+        this.userclass = userclass ; 
+        this.value = value ; 
+    }
+}
