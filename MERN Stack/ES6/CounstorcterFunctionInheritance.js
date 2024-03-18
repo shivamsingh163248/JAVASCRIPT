5dad29dd03b665660937f4bd680accf3377476db
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-18 13:19:19 +0530

    learning the inheritance in the function inheritance

--- MERN Stack/ES6/CounstorcterFunctionInheritance.js
+++ MERN Stack/ES6/CounstorcterFunctionInheritance.js
@@ -6,8 +6,33 @@ function Vehikals(VehikalsNmae , vahikalsType , vehikalNumber){
 
 
    // creating the function of the getDeials 
-   getdetails(){
+   this.getdetails = function(){
 
-   }
+    // all the details of the vehikals are printid on the console
+    console.log(`vehikals name is ${this.VehikalsNmae} and the type is ${this.vahikalsType} and the number is ${this.vehikalNumber}`) ;
+    
+ 
+   };
+
+ 
 
 }
+
+// creating another counstoctor another function 
+// and using vehikal in the class 
+
+function Car(CarName , CarHub){
+    // creating the car properties 
+    this.CarName = CarName ; 
+    this.CarHub = CarHub  ; 
+
+    // if want to user 
+    // using the call function using the this property to user like inheritance 
+    console.log(`vehikals name is ${this.VehikalsNmae} and the type is ${this.vahikalsType} and the number is ${this.vehikalNumber}`) ;
+    console.log(`CAR NAME ${this.CarName} carhub ${this.CarHub}`) ; 
+
+
+}
+
+// creating the object of the car 
+const obj = new Car("Z-20" , "toytaShow room") ; 
