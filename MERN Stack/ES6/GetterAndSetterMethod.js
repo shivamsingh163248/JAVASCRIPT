b7902006cf6189be0a6863f14c7f3559ad4785dc
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-20 07:55:12 +0530

    learning the gitter and setter method

---
+++ MERN Stack/ES6/GetterAndSetterMethod.js
@@ -0,0 +1,19 @@
+// learning the setter and getter method in the js 
+// creatin the class learning the set and get thease are the getter and and teh setter in the java secript 
+
+class Student{
+    // creating the private and public member 
+    usernaem ; 
+    userDetails ; 
+    userRollNo ; 
+
+    // creating the constructor for the setting the the value 
+    constructor(usrrName , userDetails , userRollNo){
+        this.usernaem = usrrName ; 
+        this.userDetails = userDetails ; 
+        this.userRollNo = userRollNo ; 
+    }
+
+    // creating the function 
+
+}
