58f4cd12ad79cc618a074881216854bc8c02482b
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-20 19:44:01 +0530

    learning the date abd time

---
+++ MERN Stack/ES6/DateTime.js
@@ -0,0 +1,21 @@
+// creating the Date Time 
+// creating  date time 
+const Today = new Date(); 
+ console.log(Today) ; 
+ const dob = new Date("01-08-2002") ; 
+ console.log(dob) ; 
+
+ // if a am giving input in the forment number 
+ const somday = new Date(2002, 4) ; 
+ console.log(somday) ; 
+ console.log(somday.getFullYear()) ; 
+
+
+
+// now converted to string the value 
+const stringvalue  = dob.toDateString() ; 
+const NoramlString =  dob.toString() ; 
+
+// now checking the both on the console 
+console.log(stringvalue) ; 
+console.log(NoramlString) ; 
