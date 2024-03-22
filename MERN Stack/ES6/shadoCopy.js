c7b8102dda71b24943a7042db49d550cab895cda
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-22 05:58:05 +0530

    leanring the shados copy

---
+++ MERN Stack/ES6/shadoCopy.js
@@ -0,0 +1,23 @@
+// now learning the the shaado copy 
+// that mans copy 1 single layer 
+
+
+const details1 = {
+    name : "shivam singh " , 
+    class : "b.tech" ,
+    value : true , 
+    
+    studentDetails : {
+        studentName : "itisha singh " , 
+        studentClass : "bsc"  , 
+        studentyear : 2023 , 
+    }
+
+}
+
+// now if the printing the and changes the value 
+
+const value3 = details1 ; 
+// change in the value3 then found the changed in details1 
+value3.name = "satyendra kumar singh" ; 
+console.log(details1.name) ; 
