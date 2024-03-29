e25d5a4e1ac1348c4f1978141fdea01f086fd3d8
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-29 14:44:30 +0530

    learning the Dom manupulation

--- MERN Stack/DOM/index.js
+++ MERN Stack/DOM/index.js
@@ -6,3 +6,18 @@ value.style.color = "red" ;
 // now learning the onclick function and the quarySelect and attribute selector 
 // learning the setattribute 
 
+// creating the onclick function 
+//in the button 
+// const value = document.querySelector("button").parentElement ;
+// const value1 = document.getElementById("black") ; 
+
+// now  creating the function 
+
+function changedColor(){
+    // value.setAttribute("class" , "constainer");
+    // value1.setAttribute("class" , "constainer");
+    // document.getElementById('black').setAttribute("class","const");
+   const element =  document.getElementById('black')
+    element.setAttribute("class","const");
+}
+
