9e1fb4821c36bd6e353593f1c524dd69cac49e37
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-29 08:22:11 +0530

    learning the dom manupaltion onclick setAtrribute and QuerySelector

--- MERN Stack/DOM/index.js
+++ MERN Stack/DOM/index.js
@@ -1,3 +1,8 @@
 const value = document.getElementById('nav') ; 
 // creating the change the color 
 value.style.color = "red" ; 
+
+
+// now learning the onclick function and the quarySelect and attribute selector 
+// learning the setattribute 
+
