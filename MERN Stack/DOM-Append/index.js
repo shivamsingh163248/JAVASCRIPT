3bd50dda52eea832a9c6671ae98b1ea40ae08032
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-03-30 14:00:54 +0530

    learning the append and append child method and the creating element in the js

---
+++ MERN Stack/DOM-Append/index.js
@@ -0,0 +1,17 @@
+// now creating the element and the learning appned method and append child 
+// that are the use in the js 
+const div_1 = document.createElement('div') ; 
+div_1.className = 'parent_child' ; 
+div_1.innerHTML = 'this is the parent child '
+
+// now getting the node from the 
+
+const parent = document.getElementById('parent') ; 
+const child = document.getElementById('child') ; 
+
+// just using the both and check the creating the child 
+
+parent.appendChild(div_1) ; 
+
+// again creating the next child in the child 
+
--- MERN Stack/DOM/creatingButtonInDiv.js
+++ MERN Stack/DOM/creatingButtonInDiv.js
@@ -5,7 +5,19 @@ btn.className = 'creatingButton'  ;
 
 // this is the the append in the section and div try both to creating the button 
 const sectionvalue = document.getElementById('black') ; 
-const valuesetinDiv = document.querySelector('#black div') ; 
+const valuesetinDiv = document.querySelector('div') ;
+// var children = sectionvalue.childNodes; 
 
-// sectionvalue.appendChild(btn) ; 
+
+// for (let i = 0; i < children.length; i++) {
+//     var child = children[i];
+//     if (child.nodeType === 1 && child.tagName.toLowerCase() === 'div') { // Check if the node is an element node and if it's a div
+//         child.style.backgroundColor = 'lightgray'; // For example, change the background color
+//         child.style.padding = '10px'; // Add padding
+//         child.style.marginBottom = '10px'; // Add margin bottom
+//     }
+// }
+
+
+ sectionvalue.appendChild(btn) ; 
  valuesetinDiv.appendChild(btn) ; 
