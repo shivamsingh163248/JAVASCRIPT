21fd3e0fb6b8fe6aa2b6989d21684559ec4600f0
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-04-02 17:51:46 +0530

    learning the event listoner

---
+++ MERN Stack/DOM-eventlisneor/index.js
@@ -0,0 +1,8 @@
+// creating the all the html part using the java scripts 
+const para  = document.createElement('p') ; 
+// now creating the class name of the para 
+para.innerHTML ='this is the paragraph line  my am trying to learning the the event listener' ; 
+// now adding the class name of this paragraph
+
+
+
