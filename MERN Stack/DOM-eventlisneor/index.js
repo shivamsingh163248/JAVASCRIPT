5c471642ad1ff644aac1b6ffba97916dde0662b9
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-04-02 18:05:03 +0530

    learning the eventlistener

--- MERN Stack/DOM-eventlisneor/index.js
+++ MERN Stack/DOM-eventlisneor/index.js
@@ -3,6 +3,17 @@ const para  = document.createElement('p') ;
 // now creating the class name of the para 
 para.innerHTML ='this is the paragraph line  my am trying to learning the the event listener' ; 
 // now adding the class name of this paragraph
+para.className ='para'
+// creating the setattribure
+
+para.setAttribute('id' , 'paragraph');
+
+// now paragraph the append in the parent div 
+// accessing the id using the doc id 
+const div = document.getElementById('main') ; 
+
+// now append child in the div 
+div.appendChild(para) ; 
 
 
 
