cd8e50213b883a934fb089d2d2c844e92a36c58d
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-05-30 04:17:33 +0530

    creating the var and let with the time out and test the scope

---
+++ MERN Stack/Asynchoronus-problem/timeout-problem.js
@@ -0,0 +1,13 @@
+// creating the loop and put the time out function 
+for (var i = 0; i < 5; i++) {
+    setTimeout(function(){
+  console.log(i) ; 
+    },1000)
+    
+}
+for (let i = 0; i < 5; i++) {
+    setTimeout(function(){
+  console.log(i) ; 
+    },1000)
+    
+}
