72f60068a8f13a8c43e5e636865b334d6afafb3c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-08 00:05:24 +0530

    update

--- MERN Stack/callbackAsync/callbackAsync.js
+++ MERN Stack/callbackAsync/callbackAsync.js
@@ -19,3 +19,34 @@ function welcome(){
 // now calling the function 
 result() ; 
 welcome() ; 
+
+
+
+
+
+//*************************************************** */
+
+// this function to writing the different way to use this 
+
+function result1(callback){
+    // calling the time out api 
+    setTimeout(()=>{
+
+    
+    console.log('hello world') ; 
+    callback() ; 
+    
+    },1000) ; 
+    
+    }
+    
+    // creating the next function for the for the welcome the message 
+    
+    function welcome2(){
+        console.log("welcome to learning the callback ") ; 
+    }
+    
+    // now calling the function 
+    result1(welcome2) ; 
+   
+    
