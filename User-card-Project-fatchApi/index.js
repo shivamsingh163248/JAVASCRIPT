54952adb32dc43ec155fbc46f2e0f18499dcaa9b
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-08 20:32:20 +0530

    learning AJAX AND THE learning the xmlhttps request

---
+++ User-card-Project-fatchApi/index.js
@@ -0,0 +1,41 @@
+
+// now using the xmlhttp request using the fetch the api and 
+// and showing and the using 
+
+  // using the require 
+
+
+  const request = new XMLHttpRequest() ; 
+
+
+  
+
+  // now fetch the request 
+  request.open('GET' , 'https://jsonplaceholder.typicode.com/users');
+  request.send() ; 
+  request.onload = function(){
+  const value =  request.responseText ; 
+  console.log(value) ; 
+  const myArr = JSON.parse(value) ; 
+  console.log(myArr) ; 
+  console.log(myArr.length) ; 
+ 
+
+  for (const key in myArr) {
+    
+       console.log(myArr[key].name) ; 
+        
+
+  }
+  
+  
+
+    // find the length of the response 
+    
+
+  }
+
+
+  
+
+  
