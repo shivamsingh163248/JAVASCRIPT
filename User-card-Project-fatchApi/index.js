1a823b74c78cb588584f2dc7545fd96ab7616c0c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-12 23:32:13 +0530

    learning the xmlhttp request

--- User-card-Project-fatchApi/index.js
+++ User-card-Project-fatchApi/index.js
@@ -46,15 +46,54 @@
     // jason to display the picture 
 
     // fist find the length 
-      const container = document.createElement('div') ; 
+
+      const mainContainer = document.getElementById('main-container') ; 
 
 
         for (const key in myArr) {
            
-            const id = document.createElement('div')
+            const userId = document.createElement('div')
             const name = document.createElement('div') ; 
             const username = document.createElement('div') ; 
             const email = document.createElement('div') ; 
+            const container = document.createElement('div') ;
+            const address = document.createElement('div') ; 
+
+            container.id = 'sub-container';
+
+            // creating the id of the each 
+            userId.id = 'id' ; 
+            name.id = 'name' ; 
+            username.id = 'username' ; 
+            email.id = 'email' ; 
+            address.id = 'address' ; 
+
+            // now append all the element 
+            userId.textContent = `userID : ${myArr[key].id} ` ;
+            name.textContent = `Name  : ${myArr[key].name} ` ; 
+            username.textContent = `userName : ${myArr[key].username} ` ; 
+            email.textContent = `Email : ${myArr[key].email} ` ;  
+
+            const addressObject  = myArr[key].address ; 
+            address.textContent = `address : ${addressObject.street} ${addressObject.suite} ${addressObject.city} ${addressObject.zipcode}  ` ;  
+
+            
+                
+
+            // creating the loop 
+         
+
+           console.log(addressObject);
+
+            container.appendChild(userId) ; 
+            container.appendChild(name) ; 
+            container.appendChild(username) ; 
+            container.appendChild(email) ; 
+            container.appendChild(address) ; 
+
+            mainContainer.appendChild(container) ; 
+
+
         }
 
      
