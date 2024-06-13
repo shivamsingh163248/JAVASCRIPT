63a26ce3db2edf18112300f59ee4931dcd44da22
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-13 09:01:31 +0530

    xmlhttps request

---
+++ MERN Stack/xmlhttprequest/AJAXHtyps.js
@@ -0,0 +1,17 @@
+// creating the and learning the ajax the that are using the ashynous 
+// that are the using for the call 
+
+const request = new XMLHttpRequest() ; 
+
+// creating the function for the sanding the data '
+request.open('GET' , 'https://jsonplaceholder.typicode.com/users') ; 
+// now using the sand method for the sanding the data 
+request.send()  ; 
+request.addEventListener('load' , ()=>{
+    // now printing the request in the console on the 
+   
+    console.log(request.responseText) ; 
+     // these are the change the stringify 
+     console.log(JSON.parse(request.responseText)) ; 
+
+})
