04e8f7c9e34473af90f1db93020bf4c4d0219943
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-18 23:34:14 +0530

    learning the promish

--- MERN Stack/Aschyronus-2/promish2.js
+++ MERN Stack/Aschyronus-2/promish2.js
@@ -1,4 +1,17 @@
 // creating the promish 2 that are the using 
-const result = new Promise(()=>{
+const result = new Promise((accept , reject)=>{
     // this is the arrow function 
+  // creating the set time out function 
+  setTimeout(accept("this is the my name ") , 2000) ; 
+
+
+}) ; 
+
+result.then((ans)=>{
+    console.log(ans) ; 
+}).catch((err)=>{
+  console.log(err) ; 
+})
+.finally(()=>{
+    console.log('this is the always axcute ') ; 
 })
