df02f327fc273aa353157f55757374b4daaa9706
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-19 22:06:19 +0530

    update

--- MERN Stack/Aschyronus-2/promishAll.js
+++ MERN Stack/Aschyronus-2/promishAll.js
@@ -10,6 +10,16 @@ const promise1 = new Promise((resolved)=>{
 })
 const promish2 = new Promise((resolved)=>{
     setTimeout(()=>{
-        
-    })
+        console.log("this is the resolved 2  ")
+    
+    } , 500) ; 
 })
+
+
+console.log(promise1) ; 
+console.log(promish2) ;
+
+
+
+Promise.all([promise1 , promish2 ]).then((resp)=>{console.log(resp)}) ;  
+console.log("check the full fill or not ") ; 
