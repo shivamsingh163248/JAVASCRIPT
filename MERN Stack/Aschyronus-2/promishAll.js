9dc1b5e4a52542fd13a43968ea9ec2b95ef99bf7
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-19 21:29:39 +0530

    update

---
+++ MERN Stack/Aschyronus-2/promishAll.js
@@ -0,0 +1,10 @@
+// creating the promise of all the 
+
+
+// creating the object of the all the promise 
+
+const promise1 = new Promise((resolved)=>{
+    setTimeout(()=>{
+        console.log("this is the resolved ")
+    })
+})
