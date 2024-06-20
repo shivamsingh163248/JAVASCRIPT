252fd2598a124f1b5d49d7b56b28952a59495bad
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-20 21:21:50 +0530

    update

--- MERN Stack/Aschyronus-2/PromishRacec.js
+++ MERN Stack/Aschyronus-2/PromishRacec.js
@@ -2,13 +2,27 @@
 const pro = new Promise((resolved , reject)=>{
     setTimeout(()=>{
         resolved("this is the resolve in 2000 second ") ; 
-    })
-},1000)
+    },9000)
+});
 const pro1 = new Promise((resolved , reject)=>{
     setTimeout(()=>{
-        resolved("this is the resolve in 2000 second ") ; 
-    })
+        resolved("this is the resolve in 4000 second ") ; 
+    },100)
+} ) ; 
+
+
+const result = Promise.race([pro1, pro]) ; 
+
+// n ow checking the ans 
+result.then((re)=>{
+    console.log(re) ; 
 })
 
 
-const result = Promise.race
+
+// promise resolved are the directly resolved
+
+const ans = Promise.resolve("this is the ans ") ; 
+ans.then((ans)=>{
+    console.log(ans) ; 
+})
