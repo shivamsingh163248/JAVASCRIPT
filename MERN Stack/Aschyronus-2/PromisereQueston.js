eb00d8e279a15584d39138cee9c8b6d216b8d282
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-21 23:22:50 +0530

    learning the promisce

--- MERN Stack/Aschyronus-2/PromisereQueston.js
+++ MERN Stack/Aschyronus-2/PromisereQueston.js
@@ -1,25 +1,12 @@
-function trackFitness(exercise, duration) {
-    //Implement your promise here
-    
-    return new Promise((resolve, reject) => {
-      console.log(`Tracking fitness for ${exercise},
-         duration: ${duration} minutes.`);
-  
-      setTimeout(() => {
-        if (duration > 0) {
-          resolve(duration);
-        } else {
-          reject("Invalid duration. Please provide a positive number.");
-        }
-      },5000);
+// creating the new promise and test it 
 
-    });
+const reuslt = new Promise((res , rej)=>{
+    console.log("my name is shivam singh") ; 
 
-  }
-  trackFitness("Running", 0)
-    .then((duration) => {
-      console.log(`Total duration: ${duration} minutes.`);
-    })
-    .catch((error) => {
-      console.log(`Failed to track fitness: ${error}`);
-    });
+
+})
+
+
+reuslt.then((ans)=>{
+    console.log(ans) ; 
+})
