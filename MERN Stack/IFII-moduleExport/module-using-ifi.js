a1719221708b98456cca5151d396e67f78a72237
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-30 19:42:15 +0530

    update the html file

--- MERN Stack/IFII-moduleExport/module-using-ifi.js
+++ MERN Stack/IFII-moduleExport/module-using-ifi.js
@@ -2,4 +2,32 @@
 let cart = [] ; 
 let Total = 0 ; 
 
- 
+
+// now creating the function for the return the total calculation 
+
+function totalPrice(cart){
+
+    // apply the reduce function for the getting the value 
+ Total =  cart.reduce((total , current)=>  total+current , 0);
+
+}
+
+// creating the function of the adding the element 
+// creating the three function 
+function addItem(item){
+  cart.push(item) ;
+  // then calculated the total prise 
+  totalPrice() ;  
+
+}
+
+// creating the function to get total 
+function getTotal(){
+    // creating the function 
+    return Total ; 
+}
+
+// creating the function to return the arrays 
+function getCart(){
+    return cart ; 
+}
