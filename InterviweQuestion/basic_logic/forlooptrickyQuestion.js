7f8a383bca9a8e0f73c2c1bcf75b35430b421754
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-09-21 16:15:27 +0530

    updating the values

---
+++ InterviweQuestion/basic_logic/forlooptrickyQuestion.js
@@ -0,0 +1,10 @@
+// learning the one of the basic problem of the arrays and checking the oputput 
+const arr = [3,5,6] 
+arr.foo = "shivam singh " ; 
+
+// now using the loop 
+for(let i in arr){
+    console.log(i) ; 
+}
+
+console.log(typeof[arr[3]]) ; 
