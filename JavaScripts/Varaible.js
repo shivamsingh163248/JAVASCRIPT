4e48ee26a478981f2523b1cd6c9d943a600ad982
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-05-28 19:19:21 +0530

    update

---
+++ JavaScripts/Varaible.js
@@ -0,0 +1,21 @@
+
+// let start with the variable 
+let accountId = 45 ; 
+const accountName = "shivam singh" ; 
+var accountNumber = 56556 ; 
+
+console.log(accountName) ; 
+
+// accountName = "itisha singh " ; 
+
+// now creating the scope 
+
+city = "mirzapur"
+
+{
+   var accountNumber = 565 ; 
+   let accountId = 76 ; 
+}
+
+console.log(accountNumber) ; 
+console.log(accountId)
