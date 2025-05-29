42b706c0dadcfdb9d8ef2e69c7a09dd91c75c093
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-05-29 19:09:27 +0530

    preace age case

---
+++ JavaScripts/Compresion.js
@@ -0,0 +1,17 @@
+// hear we are the learning the some adage case of the comparisons 
+console.log(null > 0) // null work as the 0
+console.log(null >= 0)
+console.log(null == 0) 
+
+
+// check for the string 
+console.log("01"==3 ) ; 
+console.log("01" < 3 ) ;
+console.log("01"> 3 ) ; 
+
+
+console.log(undefined < 0) ; 
+console.log(undefined <= 0) ; 
+console.log(undefined >= 0) ; 
+console.log(undefined == 0) ; 
+
---
+++ JavaScripts/OverationAndConversion.js
@@ -0,0 +1,62 @@
+// write all the edage case of the operation 
+let value = 3 
+let negValue = -value
+console.log(negValue)
+
+// now creating all the Arithmetic operation 
+console.log(2+3) ; 
+console.log(2-3) ; 
+console.log(2*3) ; 
+console.log(2**3) ; 
+console.log(2/3) ; 
+console.log(2%3) ; 
+console.log(Math.floor(2/3)) ; 
+const a = 411;
+const b = 2;
+const r = parseInt(a) / parseInt(b);
+console.log(r);
+
+
+// write all the operation 
+// write  in the table of all the operator in the js  with operator heading name and description  in the theory 
+
+// JavaScript Assignment Operato
+let num1 = 10 
+let num2  = 3
+
+num1+= num2 ; 
+console.log(num1) ; 
+
+// test all the edage case this is some example but test all the adge case 
+let str1 = "shivam "
+let str2 = "singh"
+
+console.log(str1+str2) ; 
+
+console.log(+true) // explain the + iin the table and explaination with the another exaple 
+console.log(+"")
+
+
+
+// expnation in the table 
+console.log('1' + 1);        // "11" → string + number = string concatenation
+console.log(1 + '1');        // "11"
+console.log('1' + 1 + 1);    // "111" → left to right: "1"+1 = "11", then "11"+1 = "111"
+console.log(1 + 1 + '1');    // "21" → 1+1 = 2, then 2+'1' = "21"
+console.log('1' + (1 + 1));  // "12" → parentheses evaluated first, 1+1=2, then "1"+2 = "12"
+
+console.log(1*54+6-2/5) ; 
+
+// also write in the different table about the prefix and suffix working 
+let gamecounter = 100 ; 
+gamecounter++ ; 
+++gamecounter ; 
+gamecounter-- ; 
+++gamecounter ; 
+console.log(gamecounter++) ; 
+console.log(gamecounter)
+console.log(--gamecounter) ; 
+console.log(gamecounter)
+
+
+console.log(+"45"+5) ; 
